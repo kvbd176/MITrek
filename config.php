257a8459d1982058e49f6ce130f87a0299ed4d4b
kvbd176 <164621930+kvbd176@users.noreply.github.com>
2025-07-09 00:23:41 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$user = "root";
-$password = "";
-$database = "meditrack"; // You should create this in phpMyAdmin
+$host = "sql311.infinityfree.com";
+$user = "if0_39424368";
+$password = "Srajee2005";
+$database = "if0_39424368_MITrek"; // You should create this in phpMyAdmin
 
 $conn = new mysqli($host, $user, $password, $database);
 
